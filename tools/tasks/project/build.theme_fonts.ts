3e7289ec88a61cd0713ed7e93685eff3ac4af105
patronov <patronov@gmail.com>
2018-04-28 18:24:21 +0100

    Add files via upload

---
+++ tools/tasks/project/build.theme_fonts.ts
@@ -0,0 +1,7 @@
+﻿import * as gulp from 'gulp';
+import Config from '../../config';
+
+export = () => {
+    return gulp.src(Config.THEME_FONTS_SRC)
+        .pipe(gulp.dest(Config.THEME_FONTS_DEST));
+};
